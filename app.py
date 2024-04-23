961fc10c5a8f6c1bdd421240b9706b0801889369
Tyler <tyleralamo15@gmail.com>
2024-04-22 22:50:38 -0700

    First commit

---
+++ app.py
@@ -0,0 +1,18 @@
+from dash import Dash, html, dash_table, dcc, callback, Input, Output
+import dash_bootstrap_components as dbc
+import pandas as pd
+import plotly.express as px
+from data import df_1
+from nav import nav
+"""
+Student Success Dashboard That Uses A Machine Learning Model To Predict Student Grades
+Developed By The LAEP Team Led By Dr. Suaray
+"""
+
+# import the and clear data
+
+# initialize app 
+
+# app structure
+
+# callback function to respond to user input
